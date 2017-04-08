4d8182c46378fb9938a2800167ead544dd422d32
tshemsedinov <timur.shemsedinov@gmail.com>
2017-04-08 04:04:31 +0300

    Add implementation on promises

---
+++ JavaScript/1-promise.js
@@ -0,0 +1,68 @@
+'use strict';
+
+class ArrayChain {
+
+  constructor(array) {
+    this._promise = Promise.resolve(array);
+  }
+
+  then(fn) {
+    return this._promise.then(fn);
+  }
+
+  catch(fn) {
+    return this._promise.catch(fn);
+  }
+
+  fetch(fn) {
+    return (this
+      .then((data) => fn(null, data))
+      .catch((err) => fn(err))
+    );
+  }
+
+  _chain(performer, fn, initial) {
+    this._promise = this._promise.then((array) => (
+      new Promise((resolve, reject) => (
+        performer(array, fn, (err, result) => (
+          (err ? reject(err) : resolve(result))
+        ), initial)
+      ))
+    ));
+  }
+
+  map(fn) {
+    this._chain(api.metasync.map, fn);
+    return this;
+  }
+
+  filter(fn) {
+    this._chain(api.metasync.filter, fn);
+    return this;
+  }
+
+  reduce(fn, initial) {
+    this._chain(api.metasync.reduce, fn, initial);
+    return this;
+  }
+
+  each(fn) {
+    this._chain(api.metasync.each, fn);
+    return this;
+  }
+
+  series(fn) {
+    this._chain(api.metasync.series, fn);
+    return this;
+  }
+
+  find(fn) {
+    this._chain(api.metasync.find, fn);
+    return this;
+  }
+
+}
+
+module.exports = {
+  for: array => new ArrayChain(array)
+};
