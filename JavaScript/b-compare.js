cb66595b121485f049edf6caa6c6379db62b26eb
tshemsedinov <timur.shemsedinov@gmail.com>
2019-01-20 10:52:31 +0200

    Use template strings

--- JavaScript/b-compare.js
+++ JavaScript/b-compare.js
@@ -6,13 +6,13 @@ global.api = {
 
 const implementations = (
   ['6-promise', '7-functor', '8-prototype', '9-build']
-    .map(name => './' + name + '.js')
+    .map(name => `./${name}.js`)
     .map(require)
 );
 
 const test = require('./a-test.js');
 
 implementations.map((chaining, i) => {
-  console.log('Implementation: #' + i);
+  console.log(`Implementation: #${i}`);
   test(chaining);
 });
